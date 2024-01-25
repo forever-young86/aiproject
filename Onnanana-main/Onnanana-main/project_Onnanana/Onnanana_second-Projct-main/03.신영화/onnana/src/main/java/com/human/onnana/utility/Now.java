f111535f3334dff5e83873245d991fde73c79c3e
forever-young86 <gloryhwa@gmail.com>
2024-01-25 18:33:33 +0900

    01/25

--- Onnanana-main/Onnanana-main/project_Onnanana/Onnanana_second-Projct-main/03.신영화/onnana/src/main/java/com/human/onnana/utility/Now.java
+++
@@ -1,9 +0,0 @@
-package com.human.onnana.utility;
-
-public class Now {
-	    public static void main(String[] args) {
-	        String chromeDriverPath = System.getProperty("webdriver.chrome.driver");
-	        System.out.println("Chrome Driver Path: " + chromeDriverPath);
-	    }
-	}
-
